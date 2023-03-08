ce1f5926f68f512c7db20bfda61af6f530e31962
propaan <legrandenzo.pro@gmail.com>
2023-03-08 16:08:11 +0100

    Updates

---
+++ precache-manifest.10a1db21c1bbd2d51dc8353b1d48f2b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d66e89a1c4050e0cf6cf",
+    "url": "/lazulis-build/static/css/main.139276c4.chunk.css"
+  },
+  {
+    "revision": "d66e89a1c4050e0cf6cf",
+    "url": "/lazulis-build/static/js/main.d66e89a1.chunk.js"
+  },
+  {
+    "revision": "685e5782aa3eab6f876b",
+    "url": "/lazulis-build/static/js/1.685e5782.chunk.js"
+  },
+  {
+    "revision": "5fb8ead5272d8c53b51c",
+    "url": "/lazulis-build/static/js/runtime~main.5fb8ead5.js"
+  },
+  {
+    "revision": "d69ae68331c5f7b44bc778dafbf42fc2",
+    "url": "/lazulis-build/static/media/streetline.d69ae683.otf"
+  },
+  {
+    "revision": "e2ed719e36373b398be7e8f43d3a9a28",
+    "url": "/lazulis-build/index.html"
+  }
+];
